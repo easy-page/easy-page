74bccbfa7fe1f9439284f7664751ff53d1974f42
pk <1457431899@qq.com>
2024-10-14 11:53:16 +0800

    为 textArea 增加 handleChange

--- packages/easy-page-antd-ui/src/components/TextArea/index.tsx
+++ packages/easy-page-antd-ui/src/components/TextArea/index.tsx
@@ -3,18 +3,29 @@ import { Input } from 'antd';
 import type { TextAreaProps as AntdTextAreaProps } from 'antd/es/input';
 import React from 'react';
 
+export type AntdTextBaseAreaProps = AntdTextAreaProps & {
+  handleChange?: (options: {
+    onChange: AntdTextAreaProps['onChange'];
+    /** 当前最新值 */
+    value: string;
+    /** 上一次的值 */
+    preValue: string;
+    frameworkProps: TextAreaProps['frameworkProps'];
+  }) => void;
+};
+
 /**
  * - 1. 定义组件 Props，一般由：UI 库组件本身 Props + 框架通用组件 Props + 自定义组件 Props 构成
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type TextAreaProps = ComponentProps<AntdTextAreaProps, any>;
+export type TextAreaProps = ComponentProps<AntdTextBaseAreaProps, any>;
 
 /**
  * - 2. 重写 FieldUIConfig，增加组件配置提示
  */
 declare module '@easy-page/react-ui/interface' {
   export interface FieldUIConfig {
-    textArea?: AntdTextAreaProps;
+    textArea?: AntdTextBaseAreaProps;
   }
 }
 /**
@@ -24,7 +35,30 @@ declare module '@easy-page/react-ui/interface' {
  */
 export const TextArea = connector(
   React.memo((props: TextAreaProps) => {
-    const { frameworkProps: _, ...baseProps } = props;
-    return <Input.TextArea {...baseProps} />;
+    const {
+      frameworkProps: _,
+      onChange,
+      value,
+      handleChange,
+      ...baseProps
+    } = props;
+    return (
+      <Input.TextArea
+        value={value}
+        onChange={(e) => {
+          if (!handleChange) {
+            onChange(e);
+            return;
+          }
+          handleChange({
+            onChange,
+            preValue: value,
+            value: e.target.value,
+            frameworkProps: props.frameworkProps,
+          });
+        }}
+        {...baseProps}
+      />
+    );
   })
 );
