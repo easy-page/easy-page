62e9a9bfdb6b525b08022bc252273b8eb6a25171
pk <1457431899@qq.com>
2025-01-09 19:12:36 +0800

    日期增加 handlechange 1.0.94

--- packages/easy-page-antd-ui/src/components/DatePickerRange/index.tsx
+++ packages/easy-page-antd-ui/src/components/DatePickerRange/index.tsx
@@ -3,18 +3,29 @@ import { DatePicker as AntdDatePicker } from 'antd';
 import type { RangePickerProps } from 'antd/es/date-picker';
 import React from 'react';
 
+export type BaseRangePickerProps = RangePickerProps & {
+  handleChange?: (options: {
+    onChange: RangePickerProps['onChange'];
+    /** 当前最新值 */
+    value: any;
+    /** 上一次的值 */
+    preValue: any;
+    frameworkProps: DatePickerRangeProps['frameworkProps'];
+  }) => void;
+};
+
 /**
  * - 1. 定义组件 Props，一般由：UI 库组件本身 Props + 框架通用组件 Props + 自定义组件 Props 构成
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type DatePickerRangeProps = ComponentProps<RangePickerProps, any>;
+export type DatePickerRangeProps = ComponentProps<BaseRangePickerProps, any>;
 
 /**
  * - 2. 重写 FieldUIConfig，增加组件配置提示
  */
 declare module '@easy-page/react-ui/interface' {
   export interface FieldUIConfig {
-    datePickerRange?: RangePickerProps;
+    datePickerRange?: BaseRangePickerProps;
   }
 }
 /**
@@ -24,8 +35,31 @@ declare module '@easy-page/react-ui/interface' {
  */
 export const DatePickerRange = connector(
   React.memo((props: DatePickerRangeProps) => {
-    const { frameworkProps: _, ...baseProps } = props;
+    const {
+      frameworkProps: _,
+      onChange,
+      value,
+      handleChange,
+      ...baseProps
+    } = props;
 
-    return <AntdDatePicker.RangePicker {...baseProps} />;
+    return (
+      <AntdDatePicker.RangePicker
+        value={value}
+        onChange={(val) => {
+          if (!handleChange) {
+            onChange(val);
+            return;
+          }
+          handleChange({
+            onChange,
+            preValue: value,
+            value: val,
+            frameworkProps: props.frameworkProps,
+          });
+        }}
+        {...baseProps}
+      />
+    );
   })
 );
